99af1de941c1d1f01663822aec3060965b402386
minsu.rob <minsu.rob@gmail.com>
2022-11-08 15:21:07 +0900

    add test code

--- src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import com.shop.constant.ItemSellStatus;
 import com.shop.entity.Item;
 import java.time.LocalDateTime;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import org.junit.jupiter.api.DisplayName;
@@ -37,20 +38,37 @@ class ItemRepositoryTest {
         Item savedItem = itemRepository.save(item);
         System.out.println(savedItem.toString());
     }
-/*
-    public void createItemList(){
-        for(int i=1;i<=10;i++){
-            Item item = new Item();
-            item.setItemNm("테스트 상품" + i);
-            item.setPrice(10000 + i);
-            item.setItemDetail("테스트 상품 상세 설명" + i);
-            item.setItemSellStatus(ItemSellStatus.SELL);
-            item.setStockNumber(100); item.setRegTime(LocalDateTime.now());
-            item.setUpdateTime(LocalDateTime.now());
-            Item savedItem = itemRepository.save(item);
+
+    @Test
+    @DisplayName("상품명 조회 테스트")
+    public void findByItemNmTest() {
+        this.createItemTest();
+        List<Item> itemList = itemRepository.findByItemNm("테스트 상품1");
+        for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("상품명, 상품상세설명 or 테스트")
+    public void findByItemNmOrItemDetailTest() {
+        this.createItemTest();
+        List<Item> itemList = itemRepository.findByItemNmOrItemDetail("테스트 상품1", "테스트 상품 상세 설명5");
+        for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("@query 상품 조회 테스트")
+    public void findByDetailTest() {
+        this.createItemTest();
+        List<Item> itemList = itemRepository.findByItemDetail("테스트 상품 상세 설명");
+        for (Item item : itemList) {
+            System.out.println(item.toString());
         }
     }
 
 
-*/
+
 }

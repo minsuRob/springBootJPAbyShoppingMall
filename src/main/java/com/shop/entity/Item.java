e637709772a7c9e4f3c5109d752fa6689bfc3f35
minsu.rob <minsu.rob@gmail.com>
2022-11-09 14:13:08 +0900

    change the original source by chpt 3

--- src/main/java/com/shop/entity/Item.java
+++ src/main/java/com/shop/entity/Item.java
@@ -1,32 +1,31 @@
 package com.shop.entity;
 
 import com.shop.constant.ItemSellStatus;
-import java.time.LocalDateTime;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Lob;
-import javax.persistence.Table;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
-@Data
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
 @Entity
 @Table(name="item")
+@Getter
+@Setter
+@ToString
 public class Item {
 
     @Id
     @Column(name="item_id")
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id; //상품코드
+    private Long id;       //상품 코드
 
     @Column(nullable = false, length = 50)
     private String itemNm; //상품명
 
-    @Column(name = "price", nullable = false)
+    @Column(name="price", nullable = false)
     private int price; //가격
 
     @Column(nullable = false)
@@ -39,7 +38,8 @@ public class Item {
     @Enumerated(EnumType.STRING)
     private ItemSellStatus itemSellStatus; //상품 판매 상태
 
-    private LocalDateTime regTime; //등록시간
+    private LocalDateTime regTime; //등록 시간
 
-    private LocalDateTime updateTime; //수정시간
+    private LocalDateTime updateTime; //수정 시간
+
 }
--- src/main/java/com/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -1,12 +1,16 @@
 package com.shop.repository;
 
 import com.shop.entity.Item;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface ItemRepository extends JpaRepository<Item, Long> {
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+
+public interface ItemRepository extends JpaRepository<Item, Long>, QuerydslPredicateExecutor<Item> {
 
     List<Item> findByItemNm(String itemNm);
 
@@ -16,13 +20,12 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
 
     List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
 
-    //JPQL 사용, DB에 독립적
     @Query("select i from Item i where i.itemDetail like " +
-        "%:itemDetail% order by i.price desc")
+            "%:itemDetail% order by i.price desc")
     List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
 
-    //기존 DB쿼리를 그대로 사용할 경우
     @Query(value="select * from item i where i.item_detail like " +
-        "%:itemDetail% order by i.price desc", nativeQuery = true)
+            "%:itemDetail% order by i.price desc", nativeQuery = true)
     List<Item> findByItemDetailByNative(@Param("itemDetail") String itemDetail);
+
 }

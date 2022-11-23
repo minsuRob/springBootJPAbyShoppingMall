9f2ecd65dfbd5e0642033caec4bdb71ddce81b53
minsu.rob <minsu.rob@gmail.com>
2022-11-23 14:47:49 +0900

    feat : 회원가입 로직 추가

--- src/main/java/com/shop/controller/MemberController.java
+++ src/main/java/com/shop/controller/MemberController.java
@@ -35,11 +35,22 @@ public class MemberController {
             Member member = Member.createMember(memberFormDto, passwordEncoder);
             memberService.saveMember(member);
         } catch (IllegalStateException e) {
-            model.addAttribute("errorMessage", e.getMessage()); 
+            model.addAttribute("errorMessage", e.getMessage());
             return "member/memberForm";
         }
 
-
         return "redirect:/";
     }
+
+    @GetMapping(value="/login")
+    public String loginMember(Model model) {
+        return "member/memberLoginForm";
+    }
+
+    @GetMapping(value="/login/error")
+    public String loginError(Model model) {
+        model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 확인해주세요.");
+        return "member/memberLoginForm";
+    }
+
 }
